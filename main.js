9af808415398c2120cc119f3bb51926f4ebedbc4
sakshamwithweb <websaksham39@gmail.com>
2025-10-14 19:41:49 +0530

    .

--- main.js
+++
@@ -1,8 +0,0 @@
-const startCam = () => {
-    const video = document.getElementById('video');
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-            video.srcObject = stream;
-        })
-    }
-};
